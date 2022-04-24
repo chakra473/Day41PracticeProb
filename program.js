417c7a0ff7f6e78b856856f7f3158a69efe7a70f
chakra473 <91408040+chakra473@users.noreply.github.com>
2022-04-24 22:33:31 +0530

    added Sum_Of_Three_Int_Zero program

--- program.js
+++ program.js
@@ -1,4 +1,9 @@
-var dice=Math.floor((Math.random()*10)%6+1);
+var arr=[2,2,-4];
 
-console.log("The random dice mumber is: ",dice)
+console.log("Elements are: ");
+for (var i=0;i<arr.length;i++){
+    console.log(arr[i]);
+}
 
+var sum=arr[0]+arr[1]+arr[2];
+console.log("addition of three elements is: ",sum)
