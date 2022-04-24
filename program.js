2128ef66954bfa6142d48ce7d20a8dcb4c7070ff
chakra473 <91408040+chakra473@users.noreply.github.com>
2022-04-24 22:27:04 +0530

    added Second_Largest_And_Smallest  program

--- program.js
+++ program.js
@@ -1,4 +1,14 @@
-var dice=Math.floor((Math.random()*10)%6+1);
+var arr=[];
 
-console.log("The random dice mumber is: ",dice)
+for (var i=0;i<10;i++){
+    arr[i]=Math.floor(Math.random()*900+100);
+}
+console.log("Random number generated are as follow:");
+for (var j=0;j<arr.length;j++){
+    console.log(arr[j]);
+}
 
+arr.sort();
+
+console.log("The second smaleest number is: ",arr[1]);
+console.log("The second largest number is: ",arr[8]);
